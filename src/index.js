f4b6d0c87128825aca6e87e950549830bfc74d44
pawel <paul.szczepkowski@gmail.com>
2017-12-30 15:03:54 +0100

    Use babel-transform-export-extensions for better exports

--- src/index.js
+++ src/index.js
@@ -1,48 +1,21 @@
 import 'normalize.css/normalize.css';
 import './tachyons/tachyons.scss';
-import { H1, H2, H3, H4, H5, H6 } from './components/Headings/Headings';
-import Aux from './components/Aux/Aux';
-import Button from './components/Button/Button';
-import Link from './components/Link/Link';
-import RouterLink from './components/Link/RouterLink';
-import Spinner from './components/Spinner/Spinner';
-import Input from './components/Form/Input/Input';
-import Select from './components/Form/Select/Select';
-import Textarea from './components/Form/Textarea/Textarea';
-import Radio from './components/Form/Radio/Radio';
-import Checkbox from './components/Form/Checkbox/Checkbox';
-import PlainButton from './components/PlainButton/PlainButton';
-import CloseBtn from './components/CloseBtn/CloseBtn';
-import MenuBtn from './components/MenuBtn/MenuBtn';
-import Paragraph from './components/Text/Paragraph/Paragraph';
-import Span from './components/Text/Span/Span';
-import Card from './components/Card/Card';
-import ColorBox from './components/ColorBox/ColorBox';
-import theme from './theme/theme';
-
-export {
-  Aux,
-  H1,
-  H2,
-  H3,
-  H4,
-  H5,
-  H6,
-  Button,
-  Link,
-  RouterLink,
-  Spinner,
-  Input,
-  Select,
-  Textarea,
-  Radio,
-  Checkbox,
-  PlainButton,
-  CloseBtn,
-  MenuBtn,
-  Paragraph,
-  Span,
-  Card,
-  ColorBox,
-  theme,
-};
+export { H1, H2, H3, H4, H5, H6 } from './components/Headings/Headings';
+export Aux from './components/Aux/Aux';
+export Button from './components/Button/Button';
+export Link from './components/Link/Link';
+export RouterLink from './components/Link/RouterLink';
+export Spinner from './components/Spinner/Spinner';
+export Input from './components/Form/Input/Input';
+export Select from './components/Form/Select/Select';
+export Textarea from './components/Form/Textarea/Textarea';
+export Radio from './components/Form/Radio/Radio';
+export Checkbox from './components/Form/Checkbox/Checkbox';
+export PlainButton from './components/PlainButton/PlainButton';
+export CloseBtn from './components/CloseBtn/CloseBtn';
+export MenuBtn from './components/MenuBtn/MenuBtn';
+export Paragraph from './components/Text/Paragraph/Paragraph';
+export Span from './components/Text/Span/Span';
+export Card from './components/Card/Card';
+export ColorBox from './components/ColorBox/ColorBox';
+export theme from './theme/theme';
