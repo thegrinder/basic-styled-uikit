e247195a3efb64c1f531f5308f2e390cd6e96d5f
thegrinder <paul.szczepkowski@gmail.com>
2018-06-30 20:43:03 +0200

    Flatten Heading theme

--- src/components/Headings/headingHelpers.js
+++ src/components/Headings/headingHelpers.js
@@ -1,7 +1,15 @@
-import { getStyle } from '../../helpers/utils';
+import { css } from 'styled-components';
 
-const getHeadingSizing = ({ theme, sizing }) => theme.uiKit.heading.sizings[sizing];
-export const headingSizingStyle = getStyle(getHeadingSizing);
+export const getHeadingFontSize = (theme, sizing) => (
+  theme.uiKit.heading.sizings[sizing]
+);
+export const headingSizingStyle = ({ theme, sizing }) => (
+  css`font-size: ${getHeadingFontSize(theme, sizing)};`
+);
 
-const getHeadingColour = ({ theme, colour }) => theme.uiKit.heading.colours[colour];
-export const headingColourStyle = getStyle(getHeadingColour);
+export const getHeadingColour = (theme, colour) => (
+  theme.uiKit.heading.colours[colour]
+);
+export const headingColourStyle = ({ theme, colour }) => (
+  css`color: ${getHeadingColour(theme, colour)};`
+);
--- src/components/Headings/headingTheme.js
+++ src/components/Headings/headingTheme.js
@@ -2,53 +2,23 @@ import { rem } from '../../helpers/utils';
 
 const headingTheme = {
   colours: {
-    white: {
-      color: '#fff',
-    },
-    lightest: {
-      color: '#ccc',
-    },
-    light: {
-      color: '#999',
-    },
-    normal: {
-      color: '#666',
-    },
-    dark: {
-      color: '#424242',
-    },
-    primary: {
-      color: '#1e87f0',
-    },
-    success: {
-      color: '#32d296',
-    },
-    warning: {
-      color: '#faa05a',
-    },
-    danger: {
-      color: '#f0506e',
-    },
+    white: '#fff',
+    lightest: '#ccc',
+    light: '#999',
+    normal: '#666',
+    dark: '#424242',
+    primary: '#1e87f0',
+    success: '#32d296',
+    warning: '#faa05a',
+    danger: '#f0506e',
   },
   sizings: {
-    h1: {
-      fontSize: rem(96),
-    },
-    h2: {
-      fontSize: rem(80),
-    },
-    h3: {
-      fontSize: rem(48),
-    },
-    h4: {
-      fontSize: rem(36),
-    },
-    h5: {
-      fontSize: rem(24),
-    },
-    h6: {
-      fontSize: rem(20),
-    },
+    h1: rem(96),
+    h2: rem(80),
+    h3: rem(48),
+    h4: rem(36),
+    h5: rem(24),
+    h6: rem(20),
   },
 };
 
