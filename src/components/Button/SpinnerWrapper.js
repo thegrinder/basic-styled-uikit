24041b6cb068a1f0033a8fcca37d7da3cad51913
thegrinder <paul.szczepkowski@gmail.com>
2019-01-06 20:16:09 +0100

    Fix positioning of the spinner inside the button

--- src/components/Button/SpinnerWrapper.js
+++ src/components/Button/SpinnerWrapper.js
@@ -6,7 +6,9 @@ const SpinnerWrapper = styled.span`
   bottom: 0;
   left: 0;
   right: 0;
-  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export default SpinnerWrapper;
