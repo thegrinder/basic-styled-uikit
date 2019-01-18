374ee2104b15b7b26176caad8d424f7a8cde3a78
thegrinder <paul.szczepkowski@gmail.com>
2019-01-18 22:31:41 +0100

    Add props table to Button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -5,11 +5,17 @@ import StyledButton from './StyledButton';
 import SpinnerWrapper from './SpinnerWrapper';
 
 const propTypes = {
+  /** submitting flag that shows a component passed to renderSpinner prop */
   submitting: bool,
+  /** one of: default, primary, danger */
   btnType: string,
+  /** one of: small, default, large */
   btnSize: string,
+  /** component showed on the left (e.g. icon) */
   left: node,
+  /** component showed on the right (e.g. icon) */
   right: node,
+  /** spinner component shown when submitting flag is true */
   renderSpinner: node,
 };
 
