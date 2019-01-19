b5f81b50cd2e70fc3ab77a7de8e552974eeab510
thegrinder <paul.szczepkowski@gmail.com>
2019-01-19 17:19:59 +0100

    Fix docz warnings

--- doczrc.js
+++ doczrc.js
@@ -1,9 +1,10 @@
 export default {
   wrapper: '../../docz/Wrapper',
+  src: './src/components',
   htmlContext: {
     head: {
       raw: [
-        '<style>.border {border-width: 1px; border-style: solid;}</style>'
+        '<style>.border {border-width: 1px; border-style: solid;}</style>',
       ],
     },
   },
