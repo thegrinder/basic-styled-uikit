282be21181207afeb0e023d69ce35595562e705f
thegrinder <paul.szczepkowski@gmail.com>
2019-01-26 21:26:33 +0100

    Add as prop to Link

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
-import { string } from 'prop-types';
+import {
+  string,
+  oneOf,
+  oneOfType,
+  func,
+} from 'prop-types';
 import { textSizingStyle } from '../Text/textHelpers';
 import { linkNormalStyle, linkHoverStyle, linkCommonStyle } from './linkHelpers';
 
@@ -9,22 +14,33 @@ const propTypes = {
   sizing: string,
   /** one of: default, muted */
   linktype: string,
+  /** rendered html tag or custom router link component */
+  as: oneOfType([oneOf(['a', 'button']), func]),
 };
 
 const StyledLink = styled.a`
-cursor: pointer;
-touch-action: manipulation;
-text-decoration: none;
-font-family: inherit;
-margin-bottom: 0;
-${linkCommonStyle}
-${textSizingStyle}
-${linkNormalStyle}
-&:hover {
-  ${linkHoverStyle}
+  margin: 0;
+  padding: 0;
+  background-color: transparent;
+  border: none;
+  overflow: visible;
+  box-sizing: border-box;
+  display: inline-block;
+  vertical-align: middle;
   outline: none;
-  text-decoration: underline;
-}
+  cursor: pointer;
+  touch-action: manipulation;
+  text-decoration: none;
+  font-family: inherit;
+  margin-bottom: 0;
+  ${linkCommonStyle}
+  ${textSizingStyle}
+  ${linkNormalStyle}
+  &:hover {
+    ${linkHoverStyle}
+    outline: none;
+    text-decoration: underline;
+  }
 `;
 
 StyledLink.propTypes = propTypes;
@@ -32,9 +48,22 @@ StyledLink.propTypes = propTypes;
 const defaultProps = {
   sizing: 'm',
   linktype: 'default',
+  as: 'a',
 };
 
-const Link = props => <StyledLink {...props} />;
+const Link = ({
+  sizing,
+  linktype,
+  as,
+  ...rest
+}) => (
+    <StyledLink
+      sizing={sizing}
+      linktype={linktype}
+      as={as}
+      {...rest}
+    />
+);
 
 Link.propTypes = propTypes;
 Link.defaultProps = defaultProps;
