c46b375a63fbd1564e72fea99eb0ea4f0e431a52
thegrinder <paul.szczepkowski@gmail.com>
2019-02-02 14:22:41 +0100

    Test Button and its subcomponents

---
+++ src/components/Button/__tests__/Button.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { render, cleanup } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import theme from '../../../theme/theme';
+import Button from '../Button';
+
+
+const children = <span>children</span>;
+
+const renderComponent = props => render(
+  <ThemeProvider theme={theme}>
+    <Button {...props}>{children}</Button>
+  </ThemeProvider>,
+);
+
+describe('<Button />', () => {
+  afterEach(cleanup);
+
+  it('should render correctly with default props', () => {
+    const { container: { firstChild }, getByText } = renderComponent();
+    const childrenElement = getByText('children');
+    expect(firstChild).toBeDefined();
+    expect(firstChild).toContainElement(childrenElement);
+    expect(firstChild).toMatchSnapshot();
+  });
+
+  it('should render correctly with custom props', () => {
+    const left = <span>left</span>;
+    const right = <span>right</span>;
+    const spinner = <span data-testid="spinner" />;
+    const { container: { firstChild }, getByText, getByTestId } = renderComponent({
+      btnType: 'danger',
+      btnSize: 's',
+      left,
+      right,
+      submitting: true,
+      renderSpinner: spinner,
+    });
+
+    const leftElement = getByText('left');
+    const rightElement = getByText('right');
+    const spinnerElement = getByTestId('spinner');
+    const childrenElement = getByText('children');
+
+    expect(firstChild).toBeDefined();
+    expect(firstChild).toContainElement(leftElement);
+    expect(firstChild).toContainElement(rightElement);
+    expect(firstChild).toContainElement(spinnerElement);
+    expect(firstChild).toContainElement(childrenElement);
+    expect(firstChild).toMatchSnapshot();
+  });
+
+  it('should render <a> tag by default', () => {
+    const { firstChild } = renderComponent({ as: 'a' }).container;
+    expect(firstChild.tagName).toEqual('A');
+    expect(firstChild).toMatchSnapshot();
+  });
+
+  it('should render render <button> tag', () => {
+    const { firstChild } = renderComponent().container;
+    expect(firstChild.tagName).toEqual('BUTTON');
+    expect(firstChild).toMatchSnapshot();
+  });
+});
