390bc0a41ada896890651f897326635868650daa
thegrinder <paul.szczepkowski@gmail.com>
2019-02-03 00:45:30 +0100

    Refactor Radio to use js styles object

--- src/components/Form/Radio/Radio.js
+++ src/components/Form/Radio/Radio.js
@@ -1,32 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
 import { bool } from 'prop-types';
-import { miscInputStyles } from '../commonFormStyles';
+import { getSharedMiscInputStyles } from '../commonFormStyles';
 
 const propTypes = {
+  /** invalid flag */
   invalid: bool,
 };
 
-const StyledRadio = styled.input.attrs({
-  type: 'radio',
-})`
-  ${miscInputStyles}
-  border-radius: 50%;
-  position: relative;
-  &:before {
-    content: '';
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    margin: auto;
-    background-color: #fff;
-    width: 25%;
-    height: 25%;
-    border-radius: 50%;
-  }
-`;
+const StyledRadio = styled.input(({ theme, invalid }) => ({
+  ...getSharedMiscInputStyles(theme, invalid),
+  borderRadius: '50%',
+  position: 'relative',
+  '&::before': {
+    content: '""',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    margin: 'auto',
+    backgroundColor: '#fff',
+    width: '30%',
+    height: '30%',
+    borderRadius: '50%',
+  },
+}));
 
 StyledRadio.propTypes = propTypes;
 
@@ -34,7 +33,9 @@ const defaultProps = {
   invalid: false,
 };
 
-const Radio = props => <StyledRadio {...props} />;
+const Radio = props => (
+  <StyledRadio type="radio" {...props} />
+);
 
 Radio.propTypes = propTypes;
 Radio.defaultProps = defaultProps;
