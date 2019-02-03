e78bc6a43404072fc6d7460578c07e41d84f4053
thegrinder <paul.szczepkowski@gmail.com>
2019-02-03 17:34:56 +0100

    Add StyledRadio tests

--- src/components/Form/Radio/Radio.js
+++ src/components/Form/Radio/Radio.js
@@ -1,34 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
 import { bool } from 'prop-types';
-import { miscInputStyles } from '../commonFormStyles';
+import StyledRadio from './StyledRadio';
 
 const propTypes = {
+  /** invalid flag */
   invalid: bool,
 };
-
-const StyledRadio = styled.input.attrs({
-  type: 'radio',
-})`
-  ${miscInputStyles}
-  border-radius: 50%;
-  position: relative;
-  &:before {
-    content: '';
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    margin: auto;
-    background-color: #fff;
-    width: 25%;
-    height: 25%;
-    border-radius: 50%;
-  }
-`;
-
-StyledRadio.propTypes = propTypes;
 
 const defaultProps = {
   invalid: false,
