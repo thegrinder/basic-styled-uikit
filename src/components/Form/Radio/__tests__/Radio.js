1f40efde0b76b71ffd991fd3442320bee5f7837a
thegrinder <paul.szczepkowski@gmail.com>
2019-02-03 12:47:21 +0100

    Test Radio

---
+++ src/components/Form/Radio/__tests__/Radio.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { render, cleanup } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import theme from '../../../../theme/theme';
+import Radio from '../Radio';
+
+
+const renderComponent = props => render(
+  <ThemeProvider theme={theme}>
+    <Radio {...props} />
+  </ThemeProvider>,
+);
+
+describe('<Radio />', () => {
+  afterEach(cleanup);
+
+  it('should render correctly with default props', () => {
+    const { container: { firstChild } } = renderComponent();
+    expect(firstChild).toBeDefined();
+    expect(firstChild).toMatchSnapshot();
+  });
+
+  it('should render correctly with custom props', () => {
+    const { container: { firstChild } } = renderComponent({
+      invalid: true,
+    });
+    expect(firstChild).toBeDefined();
+    expect(firstChild).toMatchSnapshot();
+  });
+
+  it('should render <input> tag with type radio', () => {
+    const { container: { firstChild } } = renderComponent();
+    expect(firstChild.tagName).toEqual('INPUT');
+    expect(firstChild.type).toEqual('radio');
+    expect(firstChild).toMatchSnapshot();
+  });
+});
