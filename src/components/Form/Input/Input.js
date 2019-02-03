9eca930ac816f01e24f819119d6e3c1ae8624895
thegrinder <paul.szczepkowski@gmail.com>
2019-02-03 17:29:37 +0100

    Add StyledInput tests

--- src/components/Form/Input/Input.js
+++ src/components/Form/Input/Input.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import styled from 'styled-components';
 import { bool } from 'prop-types';
-import { commonInputStyles } from '../commonFormStyles';
-import { rem } from '../../../helpers/utils';
+
+import StyledInput from './StyledInput';
 
 const propTypes = {
   /** invalid flag */
   invalid: bool,
 };
-
-const StyledInput = styled.input`
-  ${commonInputStyles}
-  vertical-align: middle;
-  display: inline-block;
-  height: ${rem(40)};
-  padding: 0 ${rem(10)};
-  overflow: visible;
-`;
-
-StyledInput.propTypes = propTypes;
 
 const defaultProps = {
   invalid: false,
---
+++ src/components/Form/Input/StyledInput.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import { bool } from 'prop-types';
+import { commonInputStyles } from '../commonFormStyles';
+import { rem } from '../../../helpers/utils';
+
+const propTypes = {
+  invalid: bool.isRequired,
+};
+
+const StyledInput = styled.input`
+  ${commonInputStyles}
+  vertical-align: middle;
+  display: inline-block;
+  height: ${rem(40)};
+  padding: 0 ${rem(10)};
+  overflow: visible;
+`;
+
+StyledInput.propTypes = propTypes;
+
+export default StyledInput;
