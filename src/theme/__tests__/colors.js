431d76ae810efdf9943fa9a252913a1b83e3b8b7
thegrinder <paul.szczepkowski@gmail.com>
2019-02-09 22:32:54 +0000

    Test colors and getColor

---
+++ src/theme/__tests__/colors.js
@@ -0,0 +1,15 @@
+import theme from '../theme';
+import colors, { getColor } from '../colors';
+
+describe('colors', () => {
+  it('should be defined', () => {
+    expect(colors).toBeDefined();
+    expect(colors).toMatchSnapshot();
+  });
+});
+
+describe('getColor', () => {
+  it('should return the right color', () => {
+    expect(getColor('primary500')({ theme })).toEqual(colors.primary500);
+  });
+});
