ea1f73b8154098a3309af276b6b5eff2404136f0
thegrinder <paul.szczepkowski@gmail.com>
2019-09-09 16:58:22 +0200

    test(formtheme): remove snapshot test

--- src/components/Form/__tests__/formTheme.js
+++ src/components/Form/__tests__/formTheme.js
@@ -3,6 +3,5 @@ import formTheme from '../formTheme';
 describe('formTheme', () => {
   it('should be defined', () => {
     expect(formTheme).toBeDefined();
-    expect(formTheme).toMatchSnapshot();
   });
 });
