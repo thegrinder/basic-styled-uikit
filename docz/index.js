bb4ae0aa220136bb4ed801ad43c0e80792eea90f
thegrinder <paul.szczepkowski@gmail.com>
2019-09-09 21:44:51 +0200

    docs: remove unused Icon helper component

--- docz/index.js
+++ docz/index.js
@@ -1,4 +1,3 @@
 export { default as Spacer } from './Spacer';
-export { default as Icon } from './Icon';
 export { default as Spinner } from './Spinner';
 export { default as InputWrapper } from './InputWrapper';
