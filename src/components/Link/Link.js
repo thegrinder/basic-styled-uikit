d6310d4950267050e9684ec8af7d842d57f83990
thegrinder <paul.szczepkowski@gmail.com>
2019-09-09 14:43:24 +0200

    chore(link): remove default props

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -11,17 +11,13 @@ const propTypes = {
   as: oneOfType([oneOf(['a', 'button']), func]),
 };
 
-const defaultProps = {
-  sizing: 'm',
-  linkType: 'default',
-  as: 'a',
-};
-
-const Link = ({ sizing, linkType: linktype, as, ...rest }) => (
-  <StyledLink sizing={sizing} linktype={linktype} as={as} {...rest} />
-);
+const Link = ({
+  sizing = 'm',
+  linkType: linktype = 'default',
+  as = 'a',
+  ...rest
+}) => <StyledLink sizing={sizing} linktype={linktype} as={as} {...rest} />;
 
 Link.propTypes = propTypes;
-Link.defaultProps = defaultProps;
 
 export default Link;
