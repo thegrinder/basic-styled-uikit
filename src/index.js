8863e174987857ed4c39ecf23b239398b888b58c
thegrinder <paul.szczepkowski@gmail.com>
2019-09-10 22:07:18 +0200

    fix(mode): export ModeProvider and useMode hook

--- src/index.js
+++ src/index.js
@@ -17,5 +17,6 @@ export {
 export { ButtonProvider, Button } from './components/Button';
 export { LinkProvider, Link } from './components/Link';
 export { ColorBoxProvider, ColorBox } from './components/ColorBox';
-export { ColorsProvider } from './components/Colors';
 export { PlainButton, ResetCss } from './components/Misc';
+export { ModeProvider, useMode } from './components/Mode';
+export { ColorsProvider } from './components/Colors';
