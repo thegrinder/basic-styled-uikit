59e3ae797de7d810fc051f6abcb01c801cecd4d0
thegrinder <paul.szczepkowski@gmail.com>
2019-09-13 09:33:00 +0200

    chore(colors): remove unnecessary colors module

--- src/components/Colors/colorsSelectors.js
+++
@@ -1,5 +0,0 @@
-import { createSelector } from '../../helpers';
-
-export const getColors = createSelector('colors');
-
-export const getColor = color => ({ theme }) => getColors(theme)[color];
--- src/index.js
+++ src/index.js
@@ -19,4 +19,3 @@ export { LinkProvider, Link } from './components/Link';
 export { BoxProvider, Box } from './components/Box';
 export { PlainButton, ResetCss } from './components/Misc';
 export { ModeProvider, useMode } from './components/Mode';
-export { ColorsProvider } from './components/Colors';
