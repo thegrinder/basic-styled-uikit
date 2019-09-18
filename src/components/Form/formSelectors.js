8bccc8f47c6099898ed93450d143ae0dcd7ec056
thegrinder <paul.szczepkowski@gmail.com>
2019-09-18 20:25:44 +0200

    refactor(form): refactor using optional chaining

--- src/components/Form/formSelectors.js
+++ src/components/Form/formSelectors.js
@@ -6,25 +6,26 @@ export const getRegularInputCommonStyle = theme =>
   getForm(theme).common && getForm(theme).common.regular;
 
 export const getRegularInputStyle = (theme, invalid, state) => {
-  const { states } = getForm(theme).regular;
-  return invalid ? states.invalid : states.valid[state];
+  const states = getForm(theme)?.regular?.states;
+  return invalid ? states?.invalid : states?.valid?.[state];
 };
 
-export const getMiscInputCommonStyle = theme =>
-  getForm(theme).common && getForm(theme).common.misc;
+export const getMiscInputCommonStyle = theme => getForm(theme)?.common?.misc;
 
 export const getMiscInputStyle = (theme, invalid, state) => {
-  const { states } = getForm(theme).misc;
-  return invalid && state === 'normal' ? states.invalid : states.valid[state];
+  const states = getForm(theme)?.misc?.states;
+  return invalid && state === 'normal'
+    ? states?.invalid
+    : states?.valid?.[state];
 };
 
 export const getInlineInputCommonStyle = theme =>
-  getForm(theme).common && getForm(theme).common.inline;
+  getForm(theme)?.common?.inline;
 
 export const getInlineInputStyle = (theme, invalid, state) => {
-  const { states } = getForm(theme).inline;
-  return invalid ? states.invalid : states.valid[state];
+  const states = getForm(theme)?.inline?.states;
+  return invalid ? states?.invalid : states?.valid?.[state];
 };
 
 export const getToggleStyle = (theme, state) =>
-  getForm(theme).toggle.states[state];
+  getForm(theme)?.toggle?.states?.[state];
