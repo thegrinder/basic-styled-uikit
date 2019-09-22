042fcbe04f351a3c2054857fccb43126b2825772
thegrinder <paul.szczepkowski@gmail.com>
2019-09-22 14:42:53 +0200

    test(select): test select styles

--- src/components/Form/Select/__tests__/Select.js
+++ src/components/Form/Select/__tests__/Select.js
@@ -1,33 +1,64 @@
 import React from 'react';
 import { render } from '../../../../test-utils';
+import FormProvider from '../../FormProvider';
 import Select from '../Select';
+
+const color = '#161616';
+const invalidColor = '#f4511e';
+const fontSize = '1rem';
+
+const theme = {
+  regular: {
+    states: {
+      valid: {
+        normal: {
+          color,
+        },
+      },
+      invalid: {
+        color: invalidColor,
+      },
+    },
+  },
+  common: {
+    regular: {
+      fontSize,
+    },
+  },
+};
 
 const testId = 'select';
 const option = <option>option</option>;
 
 const renderComponent = (props = {}) =>
   render(
-    <Select data-testid={testId} {...props}>
-      {option}
-    </Select>
+    <FormProvider theme={theme}>
+      <Select data-testid={testId} {...props}>
+        {option}
+      </Select>
+    </FormProvider>
   );
 
 describe('<Select />', () => {
-  it('should render correctly with default props and its children', () => {
-    const { queryByTestId, queryByText } = renderComponent();
-    expect(queryByTestId(testId)).toBeTruthy();
+  it('should render with default styles and its children', () => {
+    const { getByTestId, queryByText } = renderComponent();
+    const select = getByTestId(testId);
     expect(queryByText('option')).toBeTruthy();
+    expect(select).toHaveStyleRule('color', color);
+    expect(select).toHaveStyleRule('font-size', fontSize);
   });
 
-  it('should render correctly with custom props', () => {
-    const { queryByTestId } = renderComponent({
+  it('should render with invalid state styles', () => {
+    const { getByTestId } = renderComponent({
       invalid: true,
     });
-    expect(queryByTestId(testId)).toBeTruthy();
+    const select = getByTestId(testId);
+    expect(select).toHaveStyleRule('color', invalidColor);
+    expect(select).toHaveStyleRule('font-size', fontSize);
   });
 
   it('should render <select> tag', () => {
-    const { queryByTestId } = renderComponent();
-    expect(queryByTestId(testId).tagName).toEqual('SELECT');
+    const { getByTestId } = renderComponent();
+    expect(getByTestId(testId).tagName).toEqual('SELECT');
   });
 });
