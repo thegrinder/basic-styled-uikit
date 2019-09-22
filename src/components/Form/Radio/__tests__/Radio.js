82f94b72cbe5d991c51153ca5fe06d2ee749ff8a
thegrinder <paul.szczepkowski@gmail.com>
2019-09-22 14:30:57 +0200

    test(radio): test radio styles

--- src/components/Form/Radio/__tests__/Radio.js
+++ src/components/Form/Radio/__tests__/Radio.js
@@ -1,28 +1,64 @@
 import React from 'react';
 import { render } from '../../../../test-utils';
+import FormProvider from '../../FormProvider';
 import Radio from '../Radio';
 
+const color = '#ced4da';
+const checkedColor = '#1e88e5';
+const invalidColor = '#f4511e';
+const fontSize = '1rem';
+
+const theme = {
+  misc: {
+    states: {
+      valid: {
+        normal: {
+          borderColor: color,
+        },
+        checked: {
+          borderColor: checkedColor,
+        },
+      },
+      invalid: {
+        borderColor: invalidColor,
+      },
+    },
+  },
+  common: {
+    misc: {
+      fontSize,
+    },
+  },
+};
 const testId = 'radio';
 
 const renderComponent = (props = {}) =>
-  render(<Radio data-testid={testId} {...props} />);
+  render(
+    <FormProvider theme={theme}>
+      <Radio data-testid={testId} {...props} />
+    </FormProvider>
+  );
 
 describe('<Radio />', () => {
-  it('should render correctly with default props', () => {
-    const { queryByTestId } = renderComponent();
-    expect(queryByTestId(testId)).toBeTruthy();
+  it('should render with default props', () => {
+    const { getByTestId } = renderComponent();
+    const radio = getByTestId(testId);
+    expect(radio).toHaveStyleRule('border-color', color);
+    expect(radio).toHaveStyleRule('font-size', fontSize);
   });
 
-  it('should render correctly with custom props', () => {
-    const { queryByTestId } = renderComponent({
+  it('should render with invalid state styles', () => {
+    const { getByTestId } = renderComponent({
       invalid: true,
     });
-    expect(queryByTestId(testId)).toBeTruthy();
+    const radio = getByTestId(testId);
+    expect(radio).toHaveStyleRule('border-color', invalidColor);
+    expect(radio).toHaveStyleRule('font-size', fontSize);
   });
 
   it('should render <input> tag with type radio', () => {
-    const { queryByTestId } = renderComponent();
-    expect(queryByTestId(testId).tagName).toEqual('INPUT');
-    expect(queryByTestId(testId).type).toEqual('radio');
+    const { getByTestId } = renderComponent();
+    expect(getByTestId(testId).tagName).toEqual('INPUT');
+    expect(getByTestId(testId).type).toEqual('radio');
   });
 });
