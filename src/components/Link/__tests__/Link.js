c8f0cd3973800a19a9ad9d95c34d1174643bf1d0
thegrinder <paul.szczepkowski@gmail.com>
2019-09-22 13:03:47 +0200

    test(link): test link styles

--- src/components/Link/__tests__/Link.js
+++ src/components/Link/__tests__/Link.js
@@ -1,16 +1,50 @@
 import React from 'react';
 import { render } from '../../../test-utils';
+import LinkProvider from '../LinkProvider';
 import Link from '../Link';
 
+const defaultColor = '#1e88e5';
+const mutedColor = '#868e96';
+const theme = {
+  linkTypes: {
+    default: {
+      normal: {
+        color: defaultColor,
+      },
+    },
+    muted: {
+      normal: {
+        color: mutedColor,
+      },
+    },
+  },
+  common: {
+    fontFamily: `'Inter var', sans-serif`,
+  },
+};
 const children = 'children';
 
 const renderComponent = (props = {}) =>
-  render(<Link {...props}>{children}</Link>);
+  render(
+    <LinkProvider theme={theme}>
+      <Link {...props}>{children}</Link>
+    </LinkProvider>
+  );
 
 describe('<Link />', () => {
   it('should render correctly with default props and its children', () => {
     const { queryByText } = renderComponent();
-    expect(queryByText(children)).toBeTruthy();
+    const link = queryByText(children);
+    expect(link).toBeTruthy();
+    expect(link).toHaveStyleRule('color', defaultColor);
+  });
+
+  it('should render with correct styles based on custom props', () => {
+    const { queryByText } = renderComponent({
+      linkType: 'muted',
+    });
+    const link = queryByText(children);
+    expect(link).toHaveStyleRule('color', mutedColor);
   });
 
   it('should render <a> tag by default', () => {
