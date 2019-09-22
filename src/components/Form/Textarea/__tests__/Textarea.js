ad42fdaedcc7686d6ea180c161d002eb54244e4d
thegrinder <paul.szczepkowski@gmail.com>
2019-09-22 14:39:38 +0200

    test(textarea): test textarea styles

--- src/components/Form/Textarea/__tests__/Textarea.js
+++ src/components/Form/Textarea/__tests__/Textarea.js
@@ -1,27 +1,60 @@
 import React from 'react';
 import { render } from '../../../../test-utils';
+import FormProvider from '../../FormProvider';
 import Textarea from '../Textarea';
+
+const color = '#161616';
+const invalidColor = '#f4511e';
+const fontSize = '1rem';
+
+const theme = {
+  regular: {
+    states: {
+      valid: {
+        normal: {
+          color,
+        },
+      },
+      invalid: {
+        color: invalidColor,
+      },
+    },
+  },
+  common: {
+    regular: {
+      fontSize,
+    },
+  },
+};
 
 const testId = 'textarea';
 
 const renderComponent = props =>
-  render(<Textarea data-testid={testId} {...props} />);
+  render(
+    <FormProvider theme={theme}>
+      <Textarea data-testid={testId} {...props} />
+    </FormProvider>
+  );
 
 describe('<Textarea />', () => {
-  it('should render correctly with default props', () => {
-    const { queryByTestId } = renderComponent();
-    expect(queryByTestId(testId)).toBeTruthy();
+  it('should render with default styles', () => {
+    const { getByTestId } = renderComponent();
+    const textarea = getByTestId(testId);
+    expect(textarea).toHaveStyleRule('color', color);
+    expect(textarea).toHaveStyleRule('font-size', fontSize);
   });
 
-  it('should render correctly with custom props', () => {
-    const { queryByTestId } = renderComponent({
+  it('should render with invalid state styles', () => {
+    const { getByTestId } = renderComponent({
       invalid: true,
     });
-    expect(queryByTestId(testId)).toBeTruthy();
+    const textarea = getByTestId(testId);
+    expect(textarea).toHaveStyleRule('color', invalidColor);
+    expect(textarea).toHaveStyleRule('font-size', fontSize);
   });
 
   it('should render <textarea> tag', () => {
-    const { queryByTestId } = renderComponent();
-    expect(queryByTestId(testId).tagName).toEqual('TEXTAREA');
+    const { getByTestId } = renderComponent();
+    expect(getByTestId(testId).tagName).toEqual('TEXTAREA');
   });
 });
