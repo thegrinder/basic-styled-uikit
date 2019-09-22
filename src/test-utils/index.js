7204aef5ecae60e51a062bc8ff59e8be3c22c801
thegrinder <paul.szczepkowski@gmail.com>
2019-09-22 22:50:48 +0200

    chore(test-utils): remove wrappers not needed for tests

--- src/test-utils/index.js
+++ src/test-utils/index.js
@@ -2,38 +2,24 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import { node } from 'prop-types';
-import { ModeProvider } from '../components/Mode';
 import { TypographyProvider } from '../components/Typography';
-import { ButtonProvider } from '../components/Button';
 import { BoxProvider } from '../components/Box';
-import { LinkProvider } from '../components/Link';
-import { FormProvider } from '../components/Form';
 
 const propTypes = {
   children: node.isRequired,
 };
 
 const Wrapper = ({ children }) => (
-  <ModeProvider>
-    <BoxProvider>
-      <TypographyProvider>
-        <ButtonProvider>
-          <LinkProvider>
-            <FormProvider>
-              <>{children}</>
-            </FormProvider>
-          </LinkProvider>
-        </ButtonProvider>
-      </TypographyProvider>
-    </BoxProvider>
-  </ModeProvider>
+  <BoxProvider>
+    <TypographyProvider>
+      <>{children}</>
+    </TypographyProvider>
+  </BoxProvider>
 );
 
 Wrapper.propTypes = propTypes;
 
 const customRender = (ui, options) =>
   render(ui, { wrapper: Wrapper, ...options });
-
-export * from '@testing-library/react';
 
 export { customRender as render, fireEvent };
