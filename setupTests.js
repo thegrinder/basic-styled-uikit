f71e9c8b5a2a8e27cc18bf8c6c0d792b4176371a
thegrinder <paul.szczepkowski@gmail.com>
2019-09-22 11:18:09 +0200

    test(box): add missing tests

--- setupTests.js
+++ setupTests.js
@@ -1,2 +1,3 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import '@testing-library/jest-dom/extend-expect';
+import 'jest-styled-components';
--- src/components/Box/__tests__/Box.js
+++ src/components/Box/__tests__/Box.js
@@ -1,16 +1,43 @@
 import React from 'react';
 import { render } from '../../../test-utils';
+import BoxProvider from '../BoxProvider';
 import Box from '../Box';
 
 const children = 'children';
 
+const neutral = '#fff';
+const boxShadow = '0 3px 1px -2px #fff';
+const theme = {
+  bgColors: {
+    neutral,
+  },
+  elevations: {
+    1: {
+      boxShadow,
+    },
+  },
+};
+
 const renderComponent = (props = {}) =>
-  render(<Box {...props}>{children}</Box>);
+  render(
+    <BoxProvider theme={theme}>
+      <Box {...props}>{children}</Box>
+    </BoxProvider>
+  );
 
 describe('<Box />', () => {
-  it('should render correctly with default props and children', () => {
+  it('should render correctly with no props and children', () => {
     const { queryByText } = renderComponent();
     expect(queryByText(children)).toBeTruthy();
+  });
+
+  it('should render with correct styles based on the props', () => {
+    const { queryByText } = renderComponent({
+      bgColor: 'neutral',
+      elevation: 1,
+    });
+    expect(queryByText(children)).toHaveStyleRule('background-color', neutral);
+    expect(queryByText(children)).toHaveStyleRule('box-shadow', boxShadow);
   });
 
   it('should render <div> tag', () => {
