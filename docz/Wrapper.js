89de0ed2938b1a790baf9d77e16553918bbf2f15
thegrinder <paul.szczepkowski@gmail.com>
2019-09-23 11:50:23 +0200

    docs: add a note where TypographyProvider is required

--- docz/Wrapper.js
+++ docz/Wrapper.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import { node } from 'prop-types';
-import { ModeProvider } from '../src/components/Mode';
-import { TypographyProvider } from '../src/components/Typography';
-import { ButtonProvider } from '../src/components/Button';
-import { BoxProvider } from '../src/components/Box';
-import { LinkProvider } from '../src/components/Link';
-import { FormProvider } from '../src/components/Form';
 import GlobalStyle from './GlobalStyle';
 
 const propTypes = {
@@ -13,22 +7,10 @@ const propTypes = {
 };
 
 const Wrapper = ({ children }) => (
-  <ModeProvider>
-    <BoxProvider>
-      <TypographyProvider>
-        <ButtonProvider>
-          <LinkProvider>
-            <FormProvider>
-              <>
-                <GlobalStyle />
-                {children}
-              </>
-            </FormProvider>
-          </LinkProvider>
-        </ButtonProvider>
-      </TypographyProvider>
-    </BoxProvider>
-  </ModeProvider>
+  <>
+    <GlobalStyle />
+    {children}
+  </>
 );
 
 Wrapper.propTypes = propTypes;
